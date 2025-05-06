dfbad5e92edb2d83a3450eab48680ca6caf9b09c
fisheater <fisheater@disroot.org>
2025-05-06 09:38:38 -0400

    Update patch.py
    some customizations

--- patch.py
+++ patch.py
@@ -1,38 +1,54 @@
-import os
-import glob
-import shutil
+import os, glob, shutil
+
+# 1) HTML patches
+ICON = '<link rel="icon" href="custom/site_icon.png">'
+CSS_CURSOR = """
+<style>
+  /* default cursor */
+  html, body, * {
+    cursor: url("https://fisheater.peterdance.com/resources/cursor.png"), auto !important;
+  }
+  /* pointer/hover cursor */
+  a:hover, button:hover, [role="button"]:hover, [style*="cursor:pointer"] {
+    cursor: url("https://fisheater.peterdance.com/resources/cursor_select.png"), auto !important;
+  }
+  /* change secondary look color */
+  :root { --looks-secondary: green !important; }
+</style>
+"""
 
 for path in glob.glob('scratch-gui/build/*.html'):
   print(f'Patching HTML {path}')
   with open(path, 'r') as f:
     contents = f.read()
-    contents = contents.replace('</head>', '<meta name="robots" content="noindex"></head>')
-    contents = contents.replace('<link rel="manifest" href="manifest.webmanifest">', '')
+    # inject favicon + cursor+color CSS just before </head>
+    contents = contents.replace(
+      '</head>',
+      ICON + CSS_CURSOR + '</head>'
+    )
+    # your existing removals
+    contents = contents.replace(
+      '<link rel="manifest" href="manifest.webmanifest">',
+      ''
+    )
   with open(path, 'w') as f:
     f.write(contents)
 
+# 2) JS patches (unchanged)
 for path in glob.glob('scratch-gui/build/**/*.js', recursive=True):
   print(f'Patching JS {path}')
   with open(path, 'r') as f:
-    contents = f.read()
-    contents = contents.replace('https://trampoline.turbowarp.org', 'https://trampoline.turbowarp.xyz')
+    contents = f.read().replace(
+      'https://trampoline.turbowarp.org',
+      'https://trampoline.turbowarp.xyz'
+    )
   with open(path, 'w') as f:
     f.write(contents)
 
-# CUSTOMIZATIONS ----------
-
-# --- override the default dango‑cat.svg in the build output ---
-#for build_svg in glob.glob('scratch-gui/build/static/media/dango-cat.*.svg'):
-#    print(f'Patching SVG {build_svg}')
-#    shutil.copy('custom/dango-cat.svg', build_svg)
-
-# --------------------------
-
-
+# 3) cleanup + index.html swap (unchanged)
 os.remove('scratch-gui/build/sw.js')
 os.remove('scratch-gui/build/manifest.webmanifest')
 os.remove('scratch-gui/build/fullscreen.html')
 os.remove('scratch-gui/build/index.html')
-
 shutil.copy('scratch-gui/build/editor.html', 'scratch-gui/build/index.html')
-shutil.copy('robots.txt', 'scratch-gui/build/robots.txt')
+shutil.copy('robots.txt',      'scratch-gui/build/robots.txt')
